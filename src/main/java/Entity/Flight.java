93b7f889f069714c84008d6a10c89b0b8570b23b
Michael Veilis <Michael Veilis>
2016-11-24 13:56:48 +0100

    Lavet API'et

---
+++ src/main/java/Entity/Flight.java
@@ -0,0 +1,153 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entity;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+/**
+ *
+ * @author Joachim
+ */
+@Entity
+public class Flight implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String flightId;
+    private String flightNumber;
+    private String date;
+    private int numberOfSeats;
+    private double totalPrice;
+    private int traveltime;
+    private String origin;
+    private String destination;
+    
+    public Flight (String flightId, String flightNumber, String date, int numberOfSeats, double totalPrice, int traveltime, String origin, String destination) {
+        this.flightId = flightId;
+        this.flightNumber = flightNumber;
+        this.date = date;
+        this.numberOfSeats = numberOfSeats;
+        this.totalPrice = totalPrice;
+        this.traveltime = traveltime;
+        this.origin = origin;
+        this.destination = destination;
+    }
+
+    public Flight () {
+        
+    }
+
+    public String getFlightId() {
+        return flightId;
+    }
+
+    public void setFlightId(String flightId) {
+        this.flightId = flightId;
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public void setFlightNumber(String flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public void setNumberOfSeats(int numberOfSeats) {
+        this.numberOfSeats = numberOfSeats;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public int getTraveltime() {
+        return traveltime;
+    }
+
+    public void setTraveltime(int traveltime) {
+        this.traveltime = traveltime;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    
+    
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Flight)) {
+            return false;
+        }
+        Flight other = (Flight) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Entity.Flight[ id=" + id + " ]";
+    }
+    
+}
--- src/main/java/KaffeData/FlightsResource.java
+++ src/main/java/KaffeData/FlightsResource.java
@@ -5,15 +5,16 @@
  */
 package KaffeData;
 
-import javax.ws.rs.core.Context;
+import Entity.Airline;
+import Entity.Flight;
 import javax.ws.rs.core.UriInfo;
-import javax.ws.rs.Consumes;
 import com.google.gson.Gson;
+import java.util.ArrayList;
 import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.PUT;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -23,8 +24,19 @@ import javax.ws.rs.core.MediaType;
  */
 @Path("flights")
 public class FlightsResource {
-    
+
     private static final Gson gson = new Gson();
+
+    ArrayList<Flight> flightList = new ArrayList<Flight>() {
+        {
+            add(new Flight("5342-5374581236347", "KAF6666", "2016-11-22T12:00:00.000Z", 6, 67, 240, "CPH", "SXF"));
+            add(new Flight("4443-8765989054363", "KAF3466", "2016-12-03T16:00:00.000Z", 9, 239, 120, "CPH", "STN"));
+            add(new Flight("2323-8675048485030", "KAF6622", "2016-03-12T15:00:00.000Z", 4, 190, 60, "STN", "SXF"));
+            add(new Flight("8765-0101909245668", "KAF5789", "2017-01-05T12:00:00.000Z", 12, 300, 264, "STN", "CPH"));
+            add(new Flight("6464-0192837465951", "KAF1113", "2017-02-01T12:00:00.000Z", 14, 200, 210, "SXF", "CPH"));
+        }
+    };
+    
 
     @Context
     private UriInfo context;
@@ -48,72 +60,28 @@ public class FlightsResource {
     @Path("/{from}/{date}/{tickets}")
     @Produces(MediaType.APPLICATION_JSON)
     public String fromDate(@PathParam("from") String from, @PathParam("date") String date, @PathParam("tickets") int tickets) {
-        return fakedata();
+        Airline airline = new Airline("Kaffemænd Tours");
+        for(Flight x : flightList) {
+            if(x.getDate().equals(date) && x.getOrigin().equals(from)) {
+                airline.addFlight(x);
+            }
+        }
+        String res = gson.toJson(airline);
+        return res;
     }
 
     @GET // Fetches available flights from and to a specific location, given a date 
     @Path("/{from}/{to}/{date}/{tickets}")
     @Produces(MediaType.APPLICATION_JSON)
     public String fromToDateTick(@PathParam("from") String from, @PathParam("to") String to, @PathParam("date") String date, @PathParam("tickets") int tickets) {
-        return fakedata();
+        Airline airline = new Airline("Kaffemænd Tours");
+        for(Flight x : flightList) {
+            if(x.getDate().equals(date) && x.getOrigin().equals(from) && x.getDestination().equals(to)) {
+                airline.addFlight(x);
+            }
+        }
+        String res = gson.toJson(airline);
+        return res;
     }
-    
-    private String fakedata() {
-        return "{\n" +
-"    \"airline\": \"Kaffemantours\",\n" +
-"    \"flights\":[\n" +
-"        {\n" +
-"            \"flightID\": \"5342-5374581236347\",\n" +
-"            \"flightNumber\": \"KAF6666\",\n" +
-"            \"date\": \"2016-11-22T12:00:00.000Z\",\n" +
-"            \"numberOfSeats\": 6,\n" +
-"            \"totalPrice\": 67,\n" +
-"            \"traveltime\": 240,\n" +
-"            \"origin\": \"CPH\",\n" +
-"            \"destination\": \"MOW\"\n" +
-"        },\n" +
-"        {\n" +
-"            \"flightID\": \"4443-8765989054363\",\n" +
-"            \"flightNumber\": \"KAF3466\",\n" +
-"            \"date\": \"2016-12-03T16:00:00.000Z\",\n" +
-"            \"numberOfSeats\": 9,\n" +
-"            \"totalPrice\": 239,\n" +
-"            \"traveltime\": 120,\n" +
-"            \"origin\": \"OSL\",\n" +
-"            \"destination\": \"ARN\"\n" +
-"        },\n" +
-"        {\n" +
-"            \"flightID\": \"2323-8675048485030\",\n" +
-"            \"flightNumber\": \"KAF6622\",\n" +
-"            \"date\": \"2016-03-12T15:00:00.000Z\",\n" +
-"            \"numberOfSeats\": 4,\n" +
-"            \"totalPrice\": 190,\n" +
-"            \"traveltime\": 60,\n" +
-"            \"origin\": \"BAL\",\n" +
-"            \"destination\": \"AAR\"\n" +
-"        },\n" +
-"        {\n" +
-"            \"flightID\": \"8765-0101909245668\",\n" +
-"            \"flightNumber\": \"KAF5789\",\n" +
-"            \"date\": \"2017-01-05T12:00:00.000Z\",\n" +
-"            \"numberOfSeats\": 12,\n" +
-"            \"totalPrice\": 300,\n" +
-"            \"traveltime\": 264,\n" +
-"            \"origin\": \"BER\",\n" +
-"            \"destination\": \"BGK\"\n" +
-"        },\n" +
-"        {\n" +
-"            \"flightID\": \"6464-0192837465951\",\n" +
-"            \"flightNumber\": \"KAF1113\",\n" +
-"            \"date\": \"2017-02-01T12:00:00.000Z\",\n" +
-"            \"numberOfSeats\": 14,\n" +
-"            \"totalPrice\": 200,\n" +
-"            \"traveltime\": 210,\n" +
-"            \"origin\": \"DAS\",\n" +
-"            \"destination\": \"HAM\"\n" +
-"        }\n" +
-"    ]\n" +
-"}\n" +
-"";
-    }
+
 }
